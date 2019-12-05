7e23d5c594a814720a48afeb037c818694c45e57
Zachary Jacobs <skatboyza124@hotmail.com>
2019-12-05 18:19:13 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,12 @@
-# Your Code Here
+
+def map_to_negativize(x)
+ answer = []
+ index = 0
+   while index < x.length do
+   if x
+      answer << x[index] * -1
+   else
+      answer << x[index]
+   end
+index += 1
+ end
