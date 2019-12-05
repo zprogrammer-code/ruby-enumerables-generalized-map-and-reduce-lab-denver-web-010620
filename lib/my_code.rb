7d8aac0d5898ef633d8b248f5f9da93a172b074b
Zachary Jacobs <skatboyza124@hotmail.com>
2019-12-05 18:27:43 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,16 +1,3 @@
 [1,2,3].map do |n|
   n * -1
 end
- ==
- map(array){|n| n*n}
- def map_to_no_change(array)
-  index = 0 
-  new_array = []
-  while index < array.length
-  if array[index]
-    new_array << array[index]
-end
-index += 1
-end
-return new_array
-end
