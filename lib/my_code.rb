b03ee90e0eaa54275f5752cafd13729e400d7873
Zachary Jacobs <skatboyza124@hotmail.com>
2019-12-05 18:26:04 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,16 +1,4 @@
-
-def map_to_negativize(x)
- answer = []
- index = 0
-   while index < x.length do
-   if x
-      answer << x[index] * -1
-   else
-      answer << x[index]
-   end
-index += 1
- end
- 
+[1,2,3].map do
  ==
  map(array){|n| n*n}
  def map_to_no_change(array)
